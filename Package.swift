6b3cf68e9a3e0e3d637895c5b09e39b9c7b815ed
Chandler Huff <contact@bludesign.biz>
2019-05-24 11:42:12 -0400

    Fix Package Swift

--- Package.swift
+++ Package.swift
@@ -2,16 +2,10 @@ import PackageDescription
 
 var package = Package(
     name: "MongoKitten",
-    targets: [
-        Target(name: "GeoJSON"),
-        Target(name: "MongoSocket"),
-        Target(name: "ExtendedJSON"),
-        Target(name: "MongoKitten", dependencies: ["GeoJSON", "MongoSocket", "ExtendedJSON"])
-        ],
     dependencies: [
         // For MongoDB Documents
         .Package(url: "https://github.com/OpenKitten/BSON.git", versions: Version(5, 2, 0) ..< Version(6, 0, 0)),
-        
+
         // For ExtendedJSON support
         .Package(url: "https://github.com/OpenKitten/Cheetah.git", majorVersion: 2),
 
@@ -20,6 +14,12 @@ var package = Package(
 
         // Asynchronous behaviour
         .Package(url: "https://github.com/OpenKitten/Schrodinger.git", majorVersion: 1),
+    ],
+    targets: [
+        Target(name: "GeoJSON"),
+        Target(name: "MongoSocket"),
+        Target(name: "ExtendedJSON"),
+        Target(name: "MongoKitten", dependencies: ["GeoJSON", "MongoSocket", "ExtendedJSON"])
     ]
 )
 
