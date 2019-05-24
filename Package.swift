ab12946262a2875deb62da901accc0559d688ee1
Chandler Huff <contact@bludesign.biz>
2019-05-24 11:48:53 -0400

    Fix Package Swift

--- Package.swift
+++ Package.swift
@@ -28,5 +28,5 @@ var package = Package(
 
 // Provides Sockets + SSL
 #if !os(macOS) && !os(iOS)
-package.dependencies.append(.Package(url: "https://github.com/OpenKitten/KittenCTLS.git", majorVersion: 1))
+package.dependencies.append(.package(url: "https://github.com/OpenKitten/KittenCTLS.git", from: Version(1, 0, 0)))
 #endif
